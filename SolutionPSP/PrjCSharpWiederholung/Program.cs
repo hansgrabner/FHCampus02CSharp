155127489563d76eeed21f8fdd21480b9e2f8b69
johann.grabner <johann.grabner@C208-PC-LB>
2020-01-16 09:21:38 +0100

    V2

--- SolutionPSP/PrjCSharpWiederholung/Program.cs
+++ SolutionPSP/PrjCSharpWiederholung/Program.cs
@@ -10,7 +10,7 @@ namespace PrjCSharpWiederholung
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World V2");
         }
     }
 }
